253e602121bc26eab5dcdfccd7e0591883c86415
xenovoai <xenovoai@gmail.com>
2025-11-21 23:11:36 +0530

    Force Vercel redeploy - clean API structure

--- api/tests.js
+++ api/tests.js
@@ -39,6 +39,8 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  console.log('API tests endpoint called:', req.method, req.url);
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
